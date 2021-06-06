6675e4e709765ba8d8fc862ac590ba9ea7176ad5
Nao <45124890+NowNewNao@users.noreply.github.com>
2021-06-06 18:59:07 +0900

    SET dashbord

--- employees/settings.py
+++ employees/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from datetime import  timedelta
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'graphene-django',
+    'graphene_django',
     'api.apps.ApiConfig',
-    'users.apps.UserConfig',
+    'users.apps.UsersConfig',
     'corsheaders'
 ]
 
